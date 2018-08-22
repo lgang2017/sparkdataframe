6dedfa9a0f93a21ef0aa0bb60f3023efb3a19039
lgang2017 <30339510+lgang2017@users.noreply.github.com>
2018-08-22 14:13:36 +0800

    Update __init__.py

--- sparkdataframe/__init__.py
+++ sparkdataframe/__init__.py
@@ -1,2 +1,7 @@
 def start():
     print("import successful")
+def spark_concat(_list):
+    spark_df = _list[0]
+    for i in range(len(_list)-1)
+        spark_df = spark_df.unionByName(_list[i+1])
+    return spark_df
