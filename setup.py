6476d93b45bd2e14727d81a3492047b29b768674
lgang2017 <30339510+lgang2017@users.noreply.github.com>
2018-08-22 11:34:47 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='sparkdataframe',
+      version='0.1',
+      description='saprk trial information',
+      url='http://github.com/tongling/clinicaltrial',
+      author='Lishigang',
+      author_email='1074243348@qq.com',
+      license='MIT',
+      #packages=['clinicaltrial','filter'],
+      packages=setuptools.find_packages(),
+      zip_safe=False)
